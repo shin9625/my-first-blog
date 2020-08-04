c558d561a932a3459fefdd17236b3b6ea579e837
shin9625 <shin961225@gmail.com>
2020-08-04 22:08:43 +0900

    create db for comment

--- blog/migrations/0002_comment.py
+++ blog/migrations/0002_comment.py
@@ -15,12 +15,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Comment',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True, serialize=False, verbose_name='ID')),
                 ('author', models.CharField(max_length=200)),
                 ('text', models.TextField()),
                 ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
                 ('approved_comment', models.BooleanField(default=False)),
-                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='comments', to='blog.Post')),
+                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                           related_name='comments', to='blog.Post')),
             ],
         ),
     ]
